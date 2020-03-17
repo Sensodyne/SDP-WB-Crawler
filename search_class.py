33546147750273792e1034a0d1a6835feacd82e5
Sojeong <46777358+Sensodyne@users.noreply.github.com>
2020-03-17 16:28:48 +0900

    0317_4pm

--- search_class.py
+++ search_class.py
@@ -22,25 +22,35 @@ class newsSearch:
         email = self.user["email"]
         password = self.user["password"]
 
-        loginLink = driver.find_Element_by_id("SIGN-IN")
-        time.sleep(0.5)
+        signInLink = driver.find_element_by_xpath('//*[@id="netspidersosh"]/header/div[1]/div[7]/a')
+        signInLink.click()
 
-        emailLink = driver.find_element_by_id("lg_id") #emailTag
+        driver.implicitly_wait(3)
+
+        # find log in link
+        loginLink = driver.find_element_by_xpath('//*[@id="lg_login_option"]/form/div/div[3]/span')
+        loginLink.click()
+        driver.implicitly_wait(3)
+
+        # enter email
+        emailLink = driver.find_element_by_xpath('//*[@id="lg_login"]/form/div[1]/input') #emailTag
         emailLink.send_keys(email)
 
-        # click
+        continueLink = driver.find_element_by_name('lg_login')
+        continueLink.click()
         time.sleep(0.5)
 
-        passwordLink = driver.find_element_by_id("lg_password") #passwordTag
+        # enter password
+        passwordLink = driver.find_element_by_xpath('//*[@id="lg_password"]/form/div[1]/input') #passwordTag
         passwordLink.send_keys(password)
 
-        # click
+        continueLink2 = driver.find_element_by_xpath('//*[@id="lg_password"]/form/div[5]/input')
+        continueLink2.click()
         time.sleep(0.5)
 
-        access = driver.find_element_by_id("submitLoginFormBtn")
-        access.click()
-
-        time.sleep(0.5)
+        # go to the front page
+        driver.back()
+        driver.implicitly_wait(3)
 
 
     def go_crawl(self):
@@ -49,44 +59,37 @@ class newsSearch:
         driver.implicitly_wait(3)
 
         # enter the website
-        driver.get("https://economictimes.indiatimes.com/")
+        driver.get("https://renewablesnow.com/news/albania-calls-tender-for-privatisation-of-hydro-power-co-ulez-shkopet-315184/")
         driver.implicitly_wait(3)
+        ext = self.textExtractor(driver)
+        print(ext)
 
-        # log in
-        self.login(driver)
+        # # log in
+        # self.login(driver)
 
-        # search keywords
-        try:
-            navigator = driver.find_element_by_link_text("Toggle navigation")
-            navigator.click()
-        except:
-            pass
+        # # search keywords
+        # search = driver.find_element_by_name('ticker')
+        # search.send_keys('india ppp')
+        # showresult = driver.find_element_by_class_name("searchIcon")
+        # showresult.click()
 
-        search = driver.find_element_by_xpath("/html/body/app-root/user-logged-in-home/app-header-main/header/div/div/div[2]/div/app-search/div/div[1]/input")
-        search.send_keys(self.company)
-        showresult = driver.find_element_by_id("searchSubmitBtn")
-        showresult.click()
+        # driver.implicitly_wait(1)
 
-        time.sleep(0.5)
-
-        searchresult = driver.find_elements_by_partial_link_text("NYSE")
+        # searchresult = driver.find_element_by_xpath('//*[@id="leftContainer"]/div[2]/a')
+        # searchresult.click()
+        # driver.implicitly_wait(3)
 
         # get articles based on html tag
 
 
-class text_parser:
-    def __init__(self, driver):
-        self.driver = driver
-    
-    def TextExtractor(driver):
-    element = self.driver.find_element_by_class_name('Normal')
-    text = element.txt
-    if text = None:
-        text = element.get_attribute("value")
-        if text = None:
-            text = element.get_attribute("innerHTML")
-    return text
-
+    def textExtractor(self, driver):
+        element = driver.find_element_by_class_name('Normal')
+        text = element.text
+        if text == None:
+            text = element.get_attribute("value")
+            if text == None:
+                text = element.get_attribute("innerHTML")
+        return text
 
 
 
